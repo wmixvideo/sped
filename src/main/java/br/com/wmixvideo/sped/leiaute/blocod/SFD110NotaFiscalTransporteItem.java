0fa9d84b5f7dcb47881cb00a1e6c600a35cbeca2
Diego Fincatto <dfincatto@wmixvideo.com.br>
2017-12-18 15:56:10 -0200

    Criado calsse de item de transporte.

---
+++ src/main/java/br/com/wmixvideo/sped/leiaute/blocod/SFD110NotaFiscalTransporteItem.java
@@ -0,0 +1,51 @@
+package br.com.wmixvideo.sped.leiaute.blocod;
+
+import br.com.wmixvideo.sped.leiaute.SFLinha;
+import br.com.wmixvideo.sped.util.SFStringBuilder;
+import br.com.wmixvideo.sped.util.SFUtil;
+
+import java.math.BigDecimal;
+
+public class SFD110NotaFiscalTransporteItem implements SFLinha {
+
+    private int campo02NumeroItem;
+    private String campo03CodigoItem;
+    private BigDecimal campo04ValorServico;
+    private BigDecimal campo05ValorOutras;
+
+    @Override
+    public String getCampo01CodigoRegistro() {
+        return "D110";
+    }
+
+    @Override
+    public String toString() {
+        final SFStringBuilder linha = new SFStringBuilder();
+        linha.append(this.getCampo01CodigoRegistro());
+        linha.append(this.campo02NumeroItem);
+        linha.append(this.campo03CodigoItem);
+        linha.append(SFUtil.formatToString(this.campo04ValorServico));
+        linha.append(SFUtil.formatToString(this.campo05ValorOutras));
+        return linha.toString();
+    }
+
+    public SFD110NotaFiscalTransporteItem setCampo02NumeroItem(final int campo02NumeroItem) {
+        this.campo02NumeroItem = campo02NumeroItem;
+        return this;
+    }
+
+    public SFD110NotaFiscalTransporteItem setCampo03CodigoItem(final String campo03CodigoItem) {
+        this.campo03CodigoItem = campo03CodigoItem;
+        return this;
+    }
+
+    public SFD110NotaFiscalTransporteItem setCampo04ValorServico(final BigDecimal campo04ValorServico) {
+        this.campo04ValorServico = campo04ValorServico;
+        return this;
+    }
+
+    public SFD110NotaFiscalTransporteItem setCampo05ValorOutras(final BigDecimal campo05ValorOutras) {
+        this.campo05ValorOutras = campo05ValorOutras;
+        return this;
+    }
+}
